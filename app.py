fd35e6930cbf6f01a05165fb9c216a0c028b9b1c
Petr Martynenko <pmartynenko@ozon.ru>
2024-01-09 20:32:57 +0300

    added approx word count

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ def index():
         return render_template('index_empty.html')
     elif request.method == 'POST':
         original_text = request.form['text']
+        formatted_text = fix_text(original_text)
+        words_count = round(len(formatted_text) / 5)  # in typing practice average word length is 5 chars
         return render_template('index_filled.html',
                                original_text=original_text,
-                               formatted_text=fix_text(original_text))
+                               formatted_text=formatted_text,
+                               words_count=words_count)
